8dd69e59dd987acd5dcadec2fe35af38ac94e0b1
Connor <connor.ross132@gmail.com>
2023-12-07 17:32:55 +1000

    Adding Flask into the App

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://your_username:your_password@localhost/your_database'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
