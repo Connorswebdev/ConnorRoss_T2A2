fad49868de4e9d67f5bb8ebeb4d3413202147714
Connor <connor.ross132@gmail.com>
2023-12-12 12:54:51 +1000

    Changes to format, restructure code

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from extensions import db
 
 
 class User(db.Model):
+    __table__ = db.Table('user', db.metadata, extend_existing=True, autoload = True, autoload_with=db.engine)
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
