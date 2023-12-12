dbcd5434ca5d168ab7da795c40a9a1acc1173584
Connor <connor.ross132@gmail.com>
2023-12-12 14:50:10 +1000

    Restructuring models.py to be symantically correct

--- website/models.py
+++ website/models.py
@@ -1,18 +1,20 @@
 from datetime import datetime
 from website.extensions import db
+from website import db
+from flask_login import UserMixin
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __table__ = db.Table('user', db.metadata, extend_existing=True, autoload = True, autoload_with=db.engine)
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    first_name = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     allergies = db.relationship('Allergy', backref='user', lazy=True)
 
 class Allergy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    allergy_name = db.Column(db.String(50), nullable=False)
+    allergy_name = db.Column(db.String(150), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class Restaurant(db.Model):
