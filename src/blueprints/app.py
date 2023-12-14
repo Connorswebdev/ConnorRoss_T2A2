b6c1cf5c40bf19bf9305a4cd70775a81398632fa
Connor <connor.ross132@gmail.com>
2023-12-15 09:18:19 +1000

    added auth_bp and user_bp

--- src/blueprints/app.py
+++ src/blueprints/app.py
@@ -1,39 +1,42 @@
 from flask import Flask
 from init import db, ma, bcrypt, jwt
-from cli_bp import db_commands
-from routes import review_bp
-from authorisation import auth_bp
+from blueprints.cli_bp import cli_bp
+from blueprints.auth_bp import auth_bp
 
-from dotenv import load_dotenv
 import os
 #App main function that notifies Flask where all functionalities are located
-def create_app():
-
-    #Initialise app
+def setup():
     app = Flask(__name__)
 
-    #Load secrets from .env file, 
-    # inlcuding URI to connect to database which contains password,
-    # and JWT key used for encryption
-    load_dotenv()
-    URI = os.environ.get('URI')
-    SECRET_JWT_KEY = os.environ.get('SECRET_JWT_KEY')
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
+    app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
+    app.json.sort_keys = False
 
-    #Set database URI
-    app.config["SQLALCHEMY_DATABASE_URI"] = URI
-
-    #Set JWT key
-    app.config["JWT_SECRET_KEY"] = SECRET_JWT_KEY
-
-    #Initialise all the modules 
+    # Imports from init used to initialize the application
     db.init_app(app)
     ma.init_app(app)
-    bcrypt.init_app(app)
     jwt.init_app(app)
+    bcrypt.init_app(app)
 
-    #Register all routes and commands here
-    app.register_blueprint(db_commands)
-    app.register_blueprint(review_bp)
+    # The following error handlers capture errors that may occur in this app and returns an error message in a user friendly format
+    @app.errorhandler(401)
+    def unauthorized(err):
+        return {'error': str(err)}, 401
+    
+    @app.errorhandler(KeyError)
+    def key_error(err):
+        return {'error': f'The field {err} is required.'}, 400
+    
+    @app.errorhandler(ValidationError)
+    def validation_error(err):
+        return {'error': err.__dict__['messages']}, 400
+
+    # Registering blueprints to run with command "flask run"
+    app.register_blueprint(cli_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(weddings_bp)
+    app.register_blueprint(guests_bp)
+    app.register_blueprint(venues_bp)
+    app.register_blueprint(users_bp)
 
     return app
---
+++ src/blueprints/users_bp.py
@@ -0,0 +1,54 @@
+from flask import Blueprint, request
+from models.user import User, UserSchema
+from init import db, bcrypt
+from flask_jwt_extended import jwt_required
+from blueprints.auth_bp import admin_required, admin_or_owner_required
+
+# To add blueprint for users and define the prefix for all URLs in this blueprint
+users_bp = Blueprint('users', __name__, url_prefix='/users')
+
+# Creating a user is found in auth_bp.py under /register | This is for other CRUD routes.
+
+# READ: Route for getting a list of all users, excluding the password for security, this is admin access only.
+@users_bp.route('/')
+@jwt_required()
+def all_users():
+    admin_required()
+    stmt = db.select(User)
+    users = db.session.scalars(stmt)
+    return UserSchema(many=True, exclude=['password']).dump(users)
+    
+
+# UPDATE: Modifying a user's information, with the id in the URL, only accessible by admin or the user themselves.
+@users_bp.route('/<int:user_id>', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_user(user_id):
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt) # Singular scalar as only one instance is expected
+    user_info = UserSchema().load(request.json) # Applies validation rules in schema
+    if user:
+        admin_or_owner_required(user.id)
+        user.first_name = user_info.get('f_name', user.first_name) # This, and the following rows, are all optional. If not entered, then the original value will be kept.
+        user.last_name = user_info.get('l_name', user.last_name)
+        user.email = user_info.get('email', user.email)
+        user.password = bcrypt.generate_password_hash(user_info.get('password', user.password)).decode('utf-8')
+        user.is_admin = user_info.get('is_admin', user.is_admin)
+        db.session.commit() # Finalizing the change in the database, don't need to add session as nothing new was created
+        return UserSchema(exclude=['password', 'id']).dump(user) # Password is excluded for security reasons, and ID is already in the URL.
+    else:
+       return {'error': 'User not found'}, 404 # Handles error of invalid user id with a clear error message
+    
+
+# DELETE: This route allows the removal of a user by the admin or the user themselves
+@users_bp.route('/<int:user_id>', methods=['DELETE'])
+@jwt_required()
+def delete_user(user_id):
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    if user:
+       admin_or_owner_required(user.id)
+       db.session.delete(user)
+       db.session.commit() # Finalising the deletion in the database
+       return{}, 200 # Nothing returned as the user was removed, with a 200 success response
+    else:
+       return {'error': 'User not found'}, 404 # Handles error of invalid user id with a clear error message
