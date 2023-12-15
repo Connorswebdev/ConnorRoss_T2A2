2181be359d8a9fb11cd34d95246d28f749838fba
Connor <connor.ross132@gmail.com>
2023-12-15 15:13:14 +1000

    fixing bug issue

--- src/blueprints/cli_bp.py
+++ src/blueprints/cli_bp.py
@@ -5,70 +5,58 @@ from models.restaurant import Restaurant
 from models.locations import Location
 from models.city import City
 from flask import Blueprint
+from flask_sqlalchemy import SQLAlchemy
 
-# cli_bp = Blueprint('db', __name__)
+cli_bp = Blueprint('db', __name__)
 
-# This command creates the database table structures based on the imported models above
+# Command to create database tables based on the imported models
 @cli_bp.cli.command("create")
 def create_db():
-    with app.app_context():
-        db.drop_all()  # Any existing tables are first dropped to ensure a clean slate
-        db.create_all()
-        print("Tables created successfully")  # A message is printed when the database tables are successfully created
+    db.drop_all()  # Drop any existing tables to ensure a clean slate
+    db.create_all()
+    print("Tables created successfully")
 
 
-# This command inputs data into the previously created tables
+# Command to seed data into the database
 @cli_bp.cli.command("seed")
 def seed_db():
-    with app.app_context():
-        users = [
-            User(
-                first_name='admin',
-                last_name='admin',
-                email='admin@dineandwine.com',
-                password=bcrypt.generate_password_hash('admin123%').decode('utf-8'),
-                is_admin=True
-            ),
-            User(
-                first_name='John',
-                last_name='Doe',
-                email='hello@fakeemail.com',
-                password=bcrypt.generate_password_hash('Johndoe1!').decode('utf-8'),
-                is_admin=False
-            ),
-            User(
-                first_name='Robin',
-                last_name='Banks',
-                email='idonotrobbanks@mymail.com',
-                password=bcrypt.generate_password_hash('iluvmoney!').decode('utf-8'),
-                is_admin=False
+    # Seed User data
+    users = [
+        User(
+            first_name='admin',
+            last_name='admin',
+            email='admin@dineandwine.com',
+            password=bcrypt.generate_password_hash('admin123%').decode('utf-8'),
+            is_admin=True
+        ),
+        User(
+            first_name='John',
+            last_name='Doe',
+            email='hello@fakeemail.com',
+            password=bcrypt.generate_password_hash('Johndoe1!').decode('utf-8'),
+            is_admin=False
+        ),
+        User(
+            first_name='Robin',
+            last_name='Banks',
+            email='idonotrobbanks@mymail.com',
+            password=bcrypt.generate_password_hash('iluvmoney!').decode('utf-8'),
+            is_admin=False
             )
         ]
 
-        db.session.query(User).delete() # Delete any existing User data first to avoid unexpected results
-        db.session.add_all(users)
-        db.session.commit() # Users are committed 
-
+    # Delete existing User data to avoid unexpected results
+    db.session.query(User).delete()
+    db.session.add_all(users)
+    db.session.commit()
         # Only Australian locations are entered currently for demonstration purposes
     locations = [
-        Location(
-            name='Melbourne'
-        ),
-        Location(
-            name='Brisbane'
-        ),
-        Location(
-            name='Hobart'
-        ),
-        Location(
-            name='Adelaide'
-        ),
-        Location(
-            name='Sydney'
-        ),
-        Location(
-            name='Perth'
-        )
+        Location(name='Melbourne'),
+        Location(name='Brisbane'),
+        Location(name='Hobart'),
+        Location(name='Adelaide'),
+        Location(name='Sydney'),
+        Location(name='Perth')
     ]
 
     db.session.query(Location).delete() # Delete any existing location data first to avoid unexpected results
@@ -77,27 +65,27 @@ def seed_db():
 
         # Only select cities are added here for demonstration purposes
     cities = [
-            City(
+        City(
                 name='Tingalpa',
                 postcode='4173',
                 location_id=locations[0].id
             ),
-            City(
+        City(
                 name='Mayfield',
                 postcode='2304',
                 location_id=locations[1].id
             ),
-            City(
+        City(
                 name='Cocount Grove',
                 postcode='0810',
                 location_id=locations[3].id
             ),
-            City(
+        City(
                 name='Riverton',
                 postcode='6148',
                 location_id=locations[4].id
             ),
-            City(
+        City(
                 name='Prospect',
                 postcode='5082',
                 location_id=locations[2].id
@@ -108,7 +96,7 @@ def seed_db():
     db.session.add_all(cities)
     db.session.commit() # Cities are committed 
     restaurants = [
-            Restaurant(
+        Restaurant(
                 name='Lemoni Greek Cuzina',
                 street_number='1795',
                 street_name='Wynnum Rd',
@@ -136,5 +124,8 @@ def seed_db():
                 city_id=cities[2].id
             )
         ]
+    db.session.query(Restaurant).delete() # Delete any existing restaurant data first to avoid unexpected results
+    db.session.add_all(restaurants)
+    db.session.commit() # Final commit to add weddings to the database
 
     print("Models seeded")
